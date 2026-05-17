4fb5c0c927ecdac1daeac768abdc34a8c5281a79
Alex Ribeiro de Faria <135660435+alexribeirofaria@github.com>
2026-05-17 10:06:08 -0300

    chore: adiciona configurações VMX para macOS
    ### Descrição
    
    - Inclui script com ajustes de arquivo VMX para preparar execução de macOS em ambiente virtualizado.

---
+++ operations/mac-os/ Confgurações arquivo vmx para macOS X.sh
@@ -0,0 +1,20 @@
+ 
+✅ Sequioa
+	smc.version = "0"
+	board-id.reflectHost = "FALSE"
+	hw.model = "iMac20,1"
+	board-id = "Mac-AF89B6D9451A490B"
+
+
+✅ Catalina
+	hw.model = "MacBookPro14,3"
+	board-id = "Mac-551B86E5744E2388"
+
+
+
+
+| Modelo            | `hw.model`         | `board-id`               |
+| ----------------- | ------------------ | ------------------------ |
+| MacBookPro (2017) | `"MacBookPro14,3"` | `"Mac-551B86E5744E2388"` |
+| iMac (2017)       | `"iMac18,3"`       | `"Mac-BE088AF8C5EB4FA2"` |
+| MacBookAir (2017) | `"MacBookAir7,2"`  | `"Mac-937CB26E2E02BB01"` |
