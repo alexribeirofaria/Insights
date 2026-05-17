662ec4c6ae00a5502dd8a4314dc794916aca7fad
Alex Ribeiro de Faria <135660435+alexribeirofaria@github.com>
2026-05-17 10:06:08 -0300

    chore: adiciona alteração do nome de rede no macOS
    ### Descrição
    
    - Adiciona script para configurar o nome do computador na rede em ambientes macOS.

---
+++ operations/mac-os/ Mudar Nome Computador na Rede mac.sh
@@ -0,0 +1,7 @@
+✅ Mudar Nome Computador na Rede macOS X 
+
+	sudo scutil --set ComputerName "ALEX-MAC-OS"	
+	sudo scutil --set HostName "ALEX-MAC-OS"
+	sudo scutil --set LocalHostName "ALEX-MAC-OS"
+
+    dscacheutil -flushcache
